99d31e0a050c2a81cf488b74e7b1b7b8a33f9a44
kael0595 <changgyu549@gmail.com>
2024-12-02 18:09:11 +0900

    BaseEntity modify

--- src/main/java/com/example/demo/base/entity/BaseEntity.java
+++ src/main/java/com/example/demo/base/entity/BaseEntity.java
@@ -1,10 +1,7 @@
 package com.example.demo.base.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -12,7 +9,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@ToString
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @SuperBuilder(toBuilder = true)
