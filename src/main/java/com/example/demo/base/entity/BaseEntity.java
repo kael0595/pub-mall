aadcac5718c566b126fba7f449aac9067c7c0818
kael0595 <changgyu549@gmail.com>
2024-12-02 18:38:00 +0900

    BaseEntity update

--- src/main/java/com/example/demo/base/entity/BaseEntity.java
+++ src/main/java/com/example/demo/base/entity/BaseEntity.java
@@ -3,7 +3,9 @@ package com.example.demo.base.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
