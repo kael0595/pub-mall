589fc9f529c9ea1171c4f557bfdda2bdea18f263
kael0595 <changgyu549@gmail.com>
2024-12-02 18:10:42 +0900

    MemberDto add

---
+++ src/main/java/com/example/demo/base/member/dto/MemberDto.java
@@ -0,0 +1,27 @@
+package com.example.demo.base.member.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+
+    private String userId;
+
+    private String username;
+
+    private String password1;
+
+    private String password2;
+
+    private String phone;
+
+    private String email;
+
+    private String nickname;
+
+    private String addr1;
+
+    private String addr2;
+}
