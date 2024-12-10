cc0e78604a44ac6f463642bce80e0f8751f8cf83
kael0595 <changgyu549@gmail.com>
2024-12-10 15:12:10 +0900

    member entity update

--- src/main/java/com/example/demo/member/entity/Member.java
+++ src/main/java/com/example/demo/member/entity/Member.java
@@ -1,8 +1,10 @@
 package com.example.demo.member.entity;
 
 import com.example.demo.base.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,18 +19,26 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public class Member extends BaseEntity {
 
+    @NotNull
+    @Column(unique = true)
     private String username;
 
+    @NotNull
     private String name;
 
+    @NotNull
     private String password;
 
+    @NotNull
     private String phone;
 
+    @NotNull
     private String email;
 
+    @NotNull
     private String nickname;
 
+    @NotNull
     private String addr1;
 
     private String addr2;
