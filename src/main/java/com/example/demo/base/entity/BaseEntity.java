0ffdcb9410be9e0a60b30e6def48e84743ed0b43
kael0595 <changgyu549@gmail.com>
2024-12-10 16:16:29 +0900

    BaseEntity updateDt modify

--- src/main/java/com/example/demo/base/entity/BaseEntity.java
+++ src/main/java/com/example/demo/base/entity/BaseEntity.java
@@ -3,10 +3,7 @@ package com.example.demo.base.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -28,6 +25,5 @@ public class BaseEntity {
     @CreatedDate
     private LocalDateTime createDt;
 
-    @LastModifiedDate
     private LocalDateTime updateDt;
 }
