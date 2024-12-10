87b75a3bc3a93d1bba8e6a9fcf9426f0e467e5e6
kael0595 <changgyu549@gmail.com>
2024-12-10 14:23:01 +0900

    logout add

--- src/main/java/com/example/demo/base/member/controller/MemberController.java
+++ src/main/java/com/example/demo/base/member/controller/MemberController.java
@@ -1,10 +1,12 @@
 package com.example.demo.base.member.controller;
 
 import com.example.demo.base.member.dto.MemberDto;
+import com.example.demo.base.member.entity.Member;
 import com.example.demo.base.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +38,34 @@ public class MemberController {
 
         memberService.join(memberDto);
 
+        return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "member/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid MemberDto memberDto,
+                        BindingResult bindingResult,
+                        Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "member/login";
+        }
+
+        Member member = memberService.findByUsernameAndPassword(memberDto.getUsername(), memberDto.getPassword1());
+
+        if (member == null) {
+            model.addAttribute("member not found", "사용자를 찾을 수 없습니다.");
+            return "member/login";
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout() {
         return "redirect:/";
     }
 }
