750f3bd261a3a82e859677cc246c3db8224bb769
kael0595 <changgyu549@gmail.com>
2024-12-11 19:11:39 +0900

    WebMvcConfig modify for image view

--- src/main/java/com/example/demo/base/configuration/WebMvcConfig.java
+++ src/main/java/com/example/demo/base/configuration/WebMvcConfig.java
@@ -9,6 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:src/main/resources/static/uploads/");
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:src/main/resources/static/uploads/");
     }
 }
