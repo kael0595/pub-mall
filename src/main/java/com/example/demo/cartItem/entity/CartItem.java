a0bce9b759904ca807447b43ce376e77320826f9
kael0595 <changgyu549@gmail.com>
2024-12-18 20:32:25 +0900

    cartItem entity create

---
+++ src/main/java/com/example/demo/cartItem/entity/CartItem.java
@@ -0,0 +1,30 @@
+package com.example.demo.cartItem.entity;
+
+import com.example.demo.base.entity.BaseEntity;
+import com.example.demo.cart.entity.Cart;
+import com.example.demo.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
+public class CartItem extends BaseEntity {
+
+    private int amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
