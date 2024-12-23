26b34afa2e31acd9b0e550a9fedbe42ed0a0e135
kael0595 <changgyu549@gmail.com>
2024-12-23 23:33:32 +0900

    CartItemRepository findByCart(Cart cart) delete & findAllByCart(Cart cart) add

--- src/main/java/com/example/demo/cartItem/repository/CartItemRepository.java
+++ src/main/java/com/example/demo/cartItem/repository/CartItemRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.cartItem.repository;
 
+import com.example.demo.cart.entity.Cart;
 import com.example.demo.cartItem.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    List<CartItem> findAllByCart(Cart cart);
+
 }
