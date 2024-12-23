d38e1784147f81f5a1aceec933a39a823c4fee2e
kael0595 <changgyu549@gmail.com>
2024-12-23 23:30:42 +0900

    OrderController add for orderConfirmation & orderCreate

---
+++ src/main/java/com/example/demo/order/controller/OrderController.java
@@ -0,0 +1,56 @@
+package com.example.demo.order.controller;
+
+import com.example.demo.cart.service.CartService;
+import com.example.demo.cartItem.service.CartItemService;
+import com.example.demo.member.entity.Member;
+import com.example.demo.member.service.MemberService;
+import com.example.demo.order.entity.Order;
+import com.example.demo.order.service.OrderService;
+import com.example.demo.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    private final ProductService productService;
+
+    private final MemberService memberService;
+
+    private final CartService cartService;
+
+    private final CartItemService cartItemService;
+
+    @GetMapping("/confirmation/{id}")
+    public String orderConfirmation(@PathVariable("id") Long id, Model model) {
+
+        Order order = orderService.findById(id);
+        model.addAttribute("order", order);
+        return "order/confirmation";
+
+    }
+
+    @PostMapping("/create")
+    public String orderCreate(@AuthenticationPrincipal User user,
+                         @RequestParam("productId") Long productId,
+                         @RequestParam("amount") int amount) {
+
+        Member member = memberService.findByUsername(user.getUsername());
+
+        Order order = orderService.createOrder(member, productId, amount);
+
+        Long orderId = order.getId();
+
+        return "redirect:/order/confirmation/" + orderId;
+    }
+
+
+}
