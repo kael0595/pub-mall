ef0799b60a9054e12f468a70649dbd6d7a9e0db2
kael0595 <changgyu549@gmail.com>
2024-12-23 23:32:34 +0900

    CartItemService modify

--- src/main/java/com/example/demo/cartItem/service/CartItemService.java
+++ src/main/java/com/example/demo/cartItem/service/CartItemService.java
@@ -24,7 +24,7 @@ public class CartItemService {
         return cartItemRepository.save(cartItem);
     }
 
-    public List<CartItem> findAll() {
-        return cartItemRepository.findAll();
+    public List<CartItem> findAllByCart(Cart cart) {
+        return cartItemRepository.findAllByCart(cart);
     }
 }
