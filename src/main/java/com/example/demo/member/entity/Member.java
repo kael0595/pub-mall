76c8189df905422ef0eb2cd27c71d64ca93e6594
kael0595 <changgyu549@gmail.com>
2024-12-31 18:53:21 +0900

    member entity modify for social login & notice

--- src/main/java/com/example/demo/member/entity/Member.java
+++ src/main/java/com/example/demo/member/entity/Member.java
@@ -3,11 +3,10 @@ package com.example.demo.member.entity;
 import com.example.demo.base.entity.BaseEntity;
 import com.example.demo.cart.entity.Cart;
 import com.example.demo.cashLog.entity.CashLog;
+import com.example.demo.notice.entity.Notice;
 import com.example.demo.order.entity.Order;
 import com.example.demo.product.entity.Product;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,36 +24,40 @@ import java.util.List;
 @EntityListeners(AuditingEntityListener.class)
 public class Member extends BaseEntity {
 
-    @NotNull
-    @NotEmpty
-    @Column(unique = true)
+//    @NotNull
+//    @NotEmpty
+//    @Column(unique = true)
     private String username;
 
-    @NotNull
-    @NotEmpty
+//    @NotNull
+//    @NotEmpty
     private String name;
 
-    @NotNull
-    @NotEmpty
+//    @NotNull
+//    @NotEmpty
     private String password;
 
-    @NotNull
-    @NotEmpty
+//    @NotNull
+//    @NotEmpty
     private String phone;
 
-    @NotNull
-    @NotEmpty
+//    @NotNull
+//    @NotEmpty
     private String email;
 
-    @NotNull
-    @NotEmpty
+//    @NotNull
+//    @NotEmpty
     private String nickname;
 
-    @NotNull
-    @NotEmpty
     private String addr1;
 
     private String addr2;
+
+    private String provider;
+
+    private String providerId;
+
+    private String oauth2Id;
 
     @Enumerated(EnumType.STRING)
     private Grade grade;
@@ -70,9 +73,24 @@ public class Member extends BaseEntity {
     @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
     private List<CashLog> cashLogList;
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Notice> noticeList;
+
     @OneToOne
     @JoinColumn(name = "cart_id")
     private Cart cart;
 
     private LocalDateTime updateDt;
+
+    public Member(String oauth2Id, String name, String password, String email, Grade grade, String provider, String providerId) {
+
+        this.oauth2Id = oauth2Id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.grade = grade;
+        this.provider = provider;
+        this.providerId = providerId;
+
+    }
 }
