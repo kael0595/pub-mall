277799930ae590792dbc974879068d00d3893c12
kael0595 <changgyu549@gmail.com>
2024-12-31 18:54:32 +0900

    Oauth2MemberInfo add for social login

---
+++ src/main/java/com/example/demo/oauth/dto/Oauth2MemberInfo.java
@@ -0,0 +1,12 @@
+package com.example.demo.oauth.dto;
+
+public interface Oauth2MemberInfo {
+
+    String getProviderId();
+
+    String getProvider();
+
+    String getName();
+
+    String getEmail();
+}
