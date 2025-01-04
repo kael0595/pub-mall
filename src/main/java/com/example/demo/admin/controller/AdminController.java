f9861ab8fe818aa4f5950c88f739e95e201cd3dd
kael0595 <changgyu549@gmail.com>
2025-01-05 02:39:46 +0900

    noticeAdd move to NoticeController

--- src/main/java/com/example/demo/admin/controller/AdminController.java
+++ src/main/java/com/example/demo/admin/controller/AdminController.java
@@ -3,16 +3,12 @@ package com.example.demo.admin.controller;
 import com.example.demo.admin.service.AdminService;
 import com.example.demo.member.entity.Member;
 import com.example.demo.member.service.MemberService;
-import com.example.demo.notice.dto.NoticeDto;
-import com.example.demo.notice.entity.Notice;
-import com.example.demo.notice.service.NoticeService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -24,8 +20,6 @@ public class AdminController {
     private final AdminService adminService;
 
     private final MemberService memberService;
-
-    private final NoticeService noticeService;
 
     @GetMapping("/memberList")
     public String memberList(Model model) {
@@ -41,24 +35,6 @@ public class AdminController {
         Member member = memberService.findById(id);
         adminService.deleteMember(member);
         return "redirect:/admin/memberList";
-    }
-
-    @GetMapping("/notice/add")
-    public String noticeAddForm(NoticeDto noticeDto, Model model) {
-        model.addAttribute("noticeDto", noticeDto);
-        return "notice/add";
-    }
-
-    @PostMapping("/notice/add")
-    public String noticeAdd(@AuthenticationPrincipal Member member,
-                            @Valid @ModelAttribute NoticeDto noticeDto,
-                            BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "notice/add";
-        }
-
-        Notice notice = noticeService.add(noticeDto, member);
-        return "redirect:/notice/list";
     }
 
 }
--- src/main/java/com/example/demo/notice/controller/NoticeController.java
+++ src/main/java/com/example/demo/notice/controller/NoticeController.java
@@ -1,11 +1,18 @@
 package com.example.demo.notice.controller;
 
+import com.example.demo.member.entity.Member;
+import com.example.demo.notice.dto.NoticeDto;
 import com.example.demo.notice.entity.Notice;
 import com.example.demo.notice.service.NoticeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +32,24 @@ public class NoticeController {
         model.addAttribute("noticeList", noticeList);
 
         return "notice/list";
+    }
+
+    @GetMapping("/notice/add")
+    public String noticeAddForm(NoticeDto noticeDto, Model model) {
+        model.addAttribute("noticeDto", noticeDto);
+        return "notice/add";
+    }
+
+    @PostMapping("/notice/add")
+    public String noticeAdd(@AuthenticationPrincipal Member member,
+                            @Valid @ModelAttribute NoticeDto noticeDto,
+                            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "notice/add";
+        }
+
+        Notice notice = noticeService.add(noticeDto, member);
+        return "redirect:/notice/list";
     }
 
 }
