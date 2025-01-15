20794e2922cec508017231dd4e87a09172a2a6e9
kael0595 <changgyu549@gmail.com>
2025-01-15 15:04:25 +0900

    add Comment controller & service & repository

---
+++ src/main/java/com/example/demo/comment/controller/CommentController.java
@@ -0,0 +1,11 @@
+package com.example.demo.comment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/review")
+public class CommentController {
+}
