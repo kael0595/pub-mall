335c16e6016478d84dbb720b4d789f5157e29fd6
kael0595 <changgyu549@gmail.com>
2025-01-15 15:04:04 +0900

    add Comment entity

---
+++ src/main/java/com/example/demo/comment/entity/Comment.java
@@ -0,0 +1,32 @@
+package com.example.demo.comment.entity;
+
+import com.example.demo.base.entity.BaseEntity;
+import com.example.demo.member.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+public class Comment extends BaseEntity {
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime updateDt;
+}
