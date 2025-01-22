3c3798b9e659a76ab4363a51fb305c59ce413843
kael0595 <changgyu549@gmail.com>
2025-01-22 18:06:59 +0900

    modify index to output productList & noticeList

--- src/main/java/com/example/demo/main/controller/MainController.java
+++ src/main/java/com/example/demo/main/controller/MainController.java
@@ -1,13 +1,35 @@
 package com.example.demo.main.controller;
 
+import com.example.demo.notice.entity.Notice;
+import com.example.demo.notice.service.NoticeService;
+import com.example.demo.product.entity.Product;
+import com.example.demo.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final ProductService productService;
+
+    private final NoticeService noticeService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        List<Product> productList = productService.findAll();
+
+        List<Notice> noticeList = noticeService.findAll();
+
+        model.addAttribute("productList", productList);
+
+        model.addAttribute("noticeList", noticeList);
+
         return "index";
     }
 }
