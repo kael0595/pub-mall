85b5e19ac9dedd84d61beb50d3163405b930cad4
kael0595 <changgyu549@gmail.com>
2025-02-03 18:30:35 +0900

    modify order entity

--- src/main/java/com/example/demo/order/entity/Order.java
+++ src/main/java/com/example/demo/order/entity/Order.java
@@ -1,7 +1,6 @@
 package com.example.demo.order.entity;
 
 import com.example.demo.base.entity.BaseEntity;
-import com.example.demo.cartItem.entity.CartItem;
 import com.example.demo.member.entity.Member;
 import com.example.demo.orderItem.entity.OrderItem;
 import com.example.demo.product.entity.Product;
@@ -12,7 +11,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -24,14 +22,15 @@ import java.util.List;
 @Table(name = "product_order")
 public class Order extends BaseEntity {
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private List<Product> productList = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
     private List<OrderItem> orderItemList;
 
     private int amount;
